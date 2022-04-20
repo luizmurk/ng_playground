a13f605c8371396c76523664bb4a78d2dca18cec
Will Odia <will4odia@gmail.com>
2022-04-20 16:06:10 +0100

    changes

--- src/app/pages/home/main.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SideMenuComponent } from 'src/app/layout/side-menu/side-menu.component';
-import { MainComponent } from './main/main/main.component';
-
-@NgModule({
-  imports: [],
-  declarations: [MainComponent, SideMenuComponent],
-})
-export class MainModule {}
